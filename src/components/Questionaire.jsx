bd05ba928c65058642a5b826f3eae092ff5da090
Mujahid Hussain <126769603+Mujahid-h@users.noreply.github.com>
2025-08-27 23:48:19 +0500

    Update Questionaire.jsx

--- src/components/Questionaire.jsx
+++ src/components/Questionaire.jsx
@@ -43,6 +43,7 @@ export default function Checkout() {
 
     try {
       const rawJSON = b64urlToUtf8(payloadB64);
+      console.log("Payload received from woocommerce plugin: ", rawJSON)
       const hash = generateHmac(rawJSON, import.meta.env.VITE_SITE_SECRET);
 
       if (hash === sig) {
